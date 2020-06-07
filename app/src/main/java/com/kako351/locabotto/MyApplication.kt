4ae481b9af7ab84deffbbbccc5e2cdf8a039cc36
kako351 <h3.birth@gmail.com>
2020-06-07 23:18:05 +0900

    setup koin

---
+++ app/src/main/java/com/kako351/locabotto/MyApplication.kt
@@ -0,0 +1,21 @@
+package com.kako351.locabotto
+
+import android.app.Application
+import com.kako351.locabotto.di.appModule
+import com.kako351.locabotto.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+    override fun onCreate(){
+        super.onCreate()
+        // start Koin!
+        startKoin {
+            // declare used Android context
+            androidContext(this@MyApplication)
+            // declare modules
+            modules(appModule)
+            modules(viewModelModule)
+        }
+    }
+}
